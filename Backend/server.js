634fab8539fbe2431b9423b0a6122c8baea97132
Harsh Saurangpate <harsh.saurangpate@gmail.com>
2026-03-18 01:50:05 +0530

    Server Connect with DataBase

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import Groq from "groq-sdk";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -11,38 +12,45 @@ const PORT = 8080;
 app.use(express.json());
 app.use(cors());
 
-const groq = new Groq({
-  apiKey: process.env.GROQ_API_KEY,
-});
-
-app.post("/test", async (req, res) => {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
-    },
-    body: JSON.stringify({
-        model: "llama-3.1-8b-instant",
-        messages: [{
-           role: "user",
-           content: req.body.message
-        }]
-    })
-  };
-
-  try {
-    const response = await fetch("https://api.groq.com/openai/v1/chat/completions", options);
-    const data = await response.json();
-    //console.log(data.choices[0].message.content);
-    
-    res.json(data);
-
-  } catch(err) {
-    console.log(err);
-  }
-});
-
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
+  connectDB();
 });
+
+const connectDB = async() => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected with Database!");
+  } catch (err) {
+    console.log("Failed to connect with Db", err);
+  }
+}
+
+// app.post("/test", async (req, res) => {
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
+//     },
+//     body: JSON.stringify({
+//         model: "llama-3.1-8b-instant",
+//         messages: [{
+//            role: "user",
+//            content: req.body.message
+//         }]
+//     })
+//   };
+
+//   try {
+//     const response = await fetch("https://api.groq.com/openai/v1/chat/completions", options);
+//     const data = await response.json();
+//     //console.log(data.choices[0].message.content);
+    
+//     res.json(data);
+
+//   } catch(err) {
+//     console.log(err);
+//   }
+// });
+

4a3193f8b29c2ebf444121e1ec96f1d67dd38e39
Harsh Saurangpate <harsh.saurangpate@gmail.com>
2026-03-18 01:12:54 +0530

    Steup uitls with Groq-API-Response

---
+++ Backend/utils/groq.js
@@ -0,0 +1,31 @@
+import "dotenv/config";
+
+const getGroqAPIResponse = async(message) => {
+    const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${process.env.GROQ_API_KEY}`
+    },
+    body: JSON.stringify({
+        model: "llama-3.1-8b-instant",
+        messages: [{
+           role: "user",
+           content: message
+        }]
+    })
+  };
+
+  try {
+    const response = await fetch("https://api.groq.com/openai/v1/chat/completions", options);
+    const data = await response.json();
+    console.log(data.choices[0].message.content);//reply
+    
+    res.json(data);
+
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+export default getGroqAPIResponse;

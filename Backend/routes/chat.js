ca6b433be7fbb1a7d07c0f3112c83dcf7b2a0ea4
Harsh Saurangpate <harsh.saurangpate@gmail.com>
2026-03-28 16:04:35 +0530

    Edit a Delete Route

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -34,29 +34,31 @@ router.get("/thread", async(req, res) => {
 
 //Get particular Threat
 router.get("/thread/:threadId", async(req, res) => {
-    const {threadId} = req.params;
     try {
-        const thread = await Thread.findOne({thread});
+        const {threadId} = req.params;
+        const thread = await Thread.findOne({threadId});
 
         if(!thread) {
-            res.status(404).json({error: "Thread not found"});
+            return res.status(404).json({error: "Thread not found"});
         }
         res.json(thread.messages);
     } catch (err) {
-        console.log(err);
+        console.log("Thread Fetch Error:", err);
         res.status(500).json({error: "Failed to fetch threads"});
     }
 });
 
 //Delete Particular Threat
 router.delete("/thread/:threadId", async(req, res) => {
-    const {threadId} = req.params;
+    
     try {
+        const {threadId} = req.params;
         const deletedThread = await Thread.findOneAndDelete({threadId});
 
         if(!deletedThread) {
-            res.status(404).json({error: "Thread not found"});
+            return res.status(404).json({error: "Thread not found"});
         }
+        res.json({ message: "Thread deleted successfully" });
     } catch (err) {
         console.log(err);
         res.status(500).json({error: "Failed to delete threads"});
@@ -67,35 +69,43 @@ router.delete("/thread/:threadId", async(req, res) => {
 router.post("/chat", async(req, res) => {
     
     const {threadId, message} = req.body;
+    console.log("BODY:", req.body);
 
     if(!threadId || !message) {
        return res.status(400).json({error: "missing required fields" });
     }
     
     try {
+        console.log("Finding thread...");
         let thread = await Thread.findOne({threadId});
 
         if(!thread){
             //create a new thread in DB
+            console.log("Creating new thread...");
             thread = new Thread({
                 threadId,
                 title: message,
                 messages:[{role: "user", content: message}]
             });
         }else {
+            console.log("Updating thread...");
             thread.messages.push({role: "user", content: message});
         }
+        console.log("Calling AI...");
 
         const assistantReply = await getGroqAPIResponse(message);
+
+        console.log("AI Reply:", assistantReply);
 
         thread.messages.push({role: "assistant", content: assistantReply});
         thread.updatedAt = new Date();
 
+        console.log("Saving to DB...");
         await thread.save();
         res.json({reply: assistantReply});
     } catch (err) {
-        console.log(err);
-        res.status(500).json({error: "something went wrong"});
+        console.error("Real Error:",err);
+        res.status(500).json({error: err.message});
     }
 });
 
